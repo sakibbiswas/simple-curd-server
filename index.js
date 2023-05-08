6ed4a60388f00900fe0de1fb5df25319363e42bd
sakibbiswas <sakibsakib99880@gmail.com>
2023-05-08 09:26:45 +0600

    index js add

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ async function run() {
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/erroR
+    // Ensures that the client will close when you finisH/erroR
     await client.close();
   }
 }
